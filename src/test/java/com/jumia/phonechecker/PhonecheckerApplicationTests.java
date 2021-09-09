f9d70e7cb9d024ea1036afe14844b5233780290c
moamen <moamen.adel@fawry.com>
2021-09-09 22:18:49 +0200

    adding test cases.

--- src/test/java/com/jumia/phonechecker/PhonecheckerApplicationTests.java
+++ src/test/java/com/jumia/phonechecker/PhonecheckerApplicationTests.java
@@ -1,13 +1,53 @@
-//package com.jumia.phonechecker;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class PhonecheckerApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.jumia.phonechecker;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.jumia.phonechecker.entity.Customer;
+import com.jumia.phonechecker.model.CustomerModel;
+import com.jumia.phonechecker.model.NumberEnum;
+import com.jumia.phonechecker.repository.CustomerRepository;
+import com.jumia.phonechecker.service.CustomerService;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class PhonecheckerApplicationTests {
+	@Autowired
+	CustomerRepository cusomterRepository;
+	@Autowired
+	CustomerService customerService;
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void testRepo() {
+		List<Customer> customers = cusomterRepository.findAll();
+		assertThat(customers).size().isPositive();
+	}
+
+	@Test
+	void testService() {
+		NumberEnum ne = NumberEnum.CAMEROON;
+		String country = ne.getCountryName();
+		List<CustomerModel> customers = customerService.findCustomers(country, null);
+		assertThat(customers).allMatch(item -> "+237".equals(item.getCountryCode()));
+	}
+
+	@Test
+	void testApi() throws Exception {
+		 this.mockMvc.perform(MockMvcRequestBuilders.get("/customer").param("country", "Cameroon"))
+         .andExpect(MockMvcResultMatchers.status().isOk()) .andDo(MockMvcResultHandlers.print());
+         }
+
+}
